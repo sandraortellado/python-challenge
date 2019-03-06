d39b564ae3b0b90b87b51cec979317fc51bad290
sandraortellado <sjortellado@gmail.com>
2019-03-05 21:41:50 -0800

    finished

---
+++ PyBank/main.py
@@ -0,0 +1,65 @@
+#Dependencies
+import os
+import csv
+import pandas as pd
+import numpy as np
+
+
+
+#Read csv to dataframe
+csvpath = os.path.join('budget_data.csv')
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    #Determine average of changes over the entire period, as well as greatest increase, greatest decrease, and date
+
+    months = 0
+    net_total = 0
+    profit_losses = []
+    date = []
+    for row in csvreader:
+        print(row[1])
+        months += 1
+        net_total += int(row[1])
+        profit_losses.append(int(row[1]))
+        date.append(row[0])
+    average = 0
+    greatest_increase = 0
+    greatest_increase_date = ""
+    greatest_decrease = 0
+    greatest_decrease_date = ""
+    for i in range(len(profit_losses)-1):
+        change = profit_losses[i+1]-profit_losses[i]
+        average += change
+        if change > greatest_increase:
+            greatest_increase = change
+            greatest_increase_date = date[i+1]
+        if change < greatest_decrease:
+            greatest_decrease = change
+            greatest_decrease_date = date[i+1]
+    average /= (months -1)
+
+#print results
+output = f'''Financial Analysis
+  ----------------------------
+    Total Months: {months}
+    Total: ${net_total}
+    Average Change: ${average}
+    Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})
+    Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})
+'''
+print(output)
+
+#export results to txt file
+file = open('pybank_results.txt', 'w') 
+file.write(output)
+file.close()
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,22 +3,21 @@ import os
 import csv
 
 #Read csv to dataframe
-election_data = 'election_data.csv'
+#election_data = 'election_data.csv'
+csvpath = os.path.join('election_data.csv')
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     candidate_dict = {}
-    num_votes = len(csvreader)
+    num_votes = 0
     for row in csvreader:
+        num_votes += 1
         candidate = row[2]
         if candidate in candidate_dict:
             candidate_dict[candidate] += 1
@@ -27,33 +26,32 @@ with open(csvpath, newline='') as csvfile:
     candidates = candidate_dict.keys()
 
     output = f'''
-  Election Results
-  -------------------------
-  Total Votes: {num_votes}
-  -------------------------
-  '''
+    Election Results
+    -------------------------
+    Total Votes: {num_votes}
+    -------------------------
+    '''
     max_votes = 0
     winner = ""
-    
     for candidate in candidate_dict:
-        percent_vote = str((candidate_dict[candidate]/num_votes) * 100)
-        votes = str(candidate_dict[candidate])
-        candidate_results = candidate + ": "  + percent_vote + "% (" + votes + ")"
-        output = output + candidate_results
+        percent_vote = str(round((candidate_dict[candidate]/num_votes) * 100, 3))
+        votes = candidate_dict[candidate]
+        candidate_results = candidate + ": "  + percent_vote + "% (" + str(votes) + ")"
+        output = output + candidate_results + "\n    "
         if votes > max_votes:
             winner = candidate
             max_votes = votes
 
     output = output + f'''
-  -------------------------
-  Winner: {winner}
-  -------------------------
-  '''
-print(output)
+    -------------------------
+    Winner: {winner}
+    -------------------------
+    '''
+    print(output)
 
-#export results to txt file
-file = open('pypoll_results.txt', 'w') 
-file.write(output)
-file.close()
+    #export results to txt file
+    file = open('pypoll_results.txt', 'w') 
+    file.write(output)
+    file.close()
 
 
